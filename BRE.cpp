1b2354c116154812b267cb59609c92388eac24d5
Mosaferr <grzech.kowalczyk@gmail.com>
2022-06-20 22:05:13 +0200

    Tworzenie plików binarnych

--- BRE.cpp
+++ BRE.cpp
@@ -2,8 +2,30 @@
 //Grzegorz Kowalczyk, D3
 
 #include <iostream>
+#include <fstream>
 
-int main()
+using namespace std;
+
+void UtworzPlik(const string, const int, const char);
+
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+	cout << "Kalkulator BRE, v.1.0" << endl;
+	UtworzPlik("plik1.bin", 100, 0x55);
+	UtworzPlik("plik2.bin", 100, 0x45);
+	return 0;
+}
+
+void UtworzPlik(const string nazwa, const int licznik, const char znak)
+{
+	ifstream plik;
+	plik.open(nazwa);
+	if (!plik)
+	{
+		ofstream plik(nazwa);
+			for (int i = 0; i < licznik; ++i)
+			{
+				plik << znak;
+			}
+	}
 }
