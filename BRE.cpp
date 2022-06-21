e9fc7071f461177c140841e48d1ce865f8196d97
Mosaferr <grzech.kowalczyk@gmail.com>
2022-06-21 21:05:35 +0200

    Dodanie HammingDistance

--- BRE.cpp
+++ BRE.cpp
@@ -10,14 +10,30 @@ using namespace std;
 
 void ZapiszLog(const string);
 void UtworzPlik(const string, const int, const char);
+uint8_t HammingDistance(uint8_t, uint8_t);
 
 int main(int argc, char* argv[])
 {
 	cout << "Kalkulator BRE, v.1.0 \n";
 	ZapiszLog("Start programu");
-	UtworzPlik("plik1.bin", 100, 0x55);
-	UtworzPlik("plik2.bin", 100, 0x45);
-	ZapiszLog("Koniec programu");
+
+	string nazwaplik1;
+	string nazwaplik2;
+
+	if (argc != 3)
+	{
+		ZapiszLog("Nie podano plików do porównania, program tworzy je sam."); 
+		UtworzPlik("plik1.bin", 100000, 0x55);
+		UtworzPlik("plik2.bin", 100000, 0x45);
+	}
+	else
+	{
+		nazwaplik1 = argv[1];
+		nazwaplik1 = argv[2];
+
+		ZapiszLog("Processing test files");
+	}
+	ZapiszLog("Koniec programu\n");
 	return 0;
 }
 
@@ -49,9 +65,23 @@ void UtworzPlik(const string nazwa, const int licznik, const char znak)
 			plik << znak;
 		}
 		plik.close();
+		ZapiszLog("Utworzono plik binarny: " + nazwa);
 	}
 	else
 	{
 		cout << "Błąd otwarcia pliku \n";
 	}
 }
+
+uint8_t HammingDistance(uint8_t n1, uint8_t n2)
+{
+	uint8_t x = n1 ^ n2; // XOR
+	uint8_t setBits = 0;
+	while (x > 0)
+	{
+		setBits += x & 1;
+		x >>= 1;
+	}
+	return setBits;
+}
+
